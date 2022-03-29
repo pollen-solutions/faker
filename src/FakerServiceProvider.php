9b314ee1b12bd5535418430f6d3612d87c85161a
jordy-manner <jordy.manner@milkcreation.fr>
2022-03-29 15:28:00 +0200

    feat: evolution config

--- src/FakerServiceProvider.php
+++ src/FakerServiceProvider.php
@@ -4,9 +4,13 @@ declare(strict_types=1);
 
 namespace Pollen\Faker;
 
-use Pollen\Container\ServiceProvider;
+use Pollen\Container\BootableServiceProvider;
+use Pollen\Event\EventDispatcherInterface;
+use Pollen\Kernel\Events\ConfigLoadEvent;
+use Psr\Container\ContainerExceptionInterface;
+use Psr\Container\NotFoundExceptionInterface;
 
-class FakerServiceProvider extends ServiceProvider
+class FakerServiceProvider extends BootableServiceProvider
 {
     protected $provides = [
         FakerInterface::class
@@ -15,10 +19,29 @@ class FakerServiceProvider extends ServiceProvider
     /**
      * @inheritDoc
      */
+    public function boot(): void
+    {
+        try {
+            /** @var EventDispatcherInterface $event */
+            if ($event = $this->getContainer()->get(EventDispatcherInterface::class)) {
+                $event->subscribeTo('config.load', static function (ConfigLoadEvent $event) {
+                    if (is_callable($config = $event->getConfig('faker'))) {
+                        $config($event->getApp()->get(FakerInterface::class), $event->getApp());
+                    }
+                });
+            }
+        } catch (NotFoundExceptionInterface|ContainerExceptionInterface $e) {
+            unset($e);
+        }
+    }
+
+    /**
+     * @inheritDoc
+     */
     public function register(): void
     {
         $this->getContainer()->share(FakerInterface::class, function () {
-            return new Faker([], $this->getContainer());
+            return new Faker($this->getContainer());
         });
     }
 }
